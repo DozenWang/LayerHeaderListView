c978897e1e59b575309e87221973ea55c295f924
wangyida <yida@watchkong.com>
2015-05-19 12:08:57 +0800

    :wq
    :

---
+++ app/src/main/java/yida/demoapplication/MainActivity.java
@@ -0,0 +1,31 @@
+package yida.demoapplication;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    PullToZoomListView listView;
+    private String[] adapterData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = (PullToZoomListView)findViewById(R.id.listview);
+        adapterData = new String[] { "Activity","Service","Content Provider","Intent","BroadcastReceiver","ADT","Sqlite3","HttpClient","DDMS","Android Studio","Fragment","Loader" };
+
+        listView.setAdapter(new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, adapterData));
+        listView.getHeaderView().setImageResource(R.drawable.splash01);
+        listView.getHeaderView().setScaleType(ImageView.ScaleType.CENTER_CROP);
+    }
+
+
+
+}
